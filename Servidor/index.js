e4cf98a942034cfe10ca2f2517b1b914fdb0592b
Sebas-AP <almedasebastian412@hotmail.com>
2025-09-29 12:34:39 -0600

    crear el servidor de sockets

---
+++ Servidor/index.js
@@ -0,0 +1,14 @@
+const net = require("net");
+const server = net.createServer();
+//encender el servidor de sockets
+server.on("connection", (socket) => {
+  socket.on("data", (data) => {
+    //da un mensaje cuando el cliente envia un mensaje
+    console.log("Mensaje recibido del cliente: ", data); //escribe en la consola
+    socket.write("Mensaje recibido \n"); //escribe en la consola del cliente
+  });
+});
+//levanta el servidor
+server.listen(3000, () => {
+  console.log("Servidor escuchando en el puerto 3000", server.address().port);
+});
